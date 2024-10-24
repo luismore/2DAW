034a5d206e5ceaaa7780898ebf5dec925a42dfc9
Luis Calvo <luiscalvomain@gmail.com>
2024-10-24 10:06:51 +0200

    Events ej2

---
+++ DEC/Eventos/ej1.js
@@ -0,0 +1,3 @@
+function saltaAlert(){
+    alert("Hola");
+}
--- DEC/Eventos/index.js
+++
@@ -1,5 +0,0 @@
-const btn = document.getElementsByTagName("button");
-
-btn[0].addEventListener("click", ()=>document.body.style.backgroundColor = "red");
-btn[1].addEventListener("click", ()=>document.body.style.backgroundColor = "blue");
-
