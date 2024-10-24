488c42f205cb5f8f62df85abda7ecfebe2667d66
Luis Calvo <luiscalvomain@gmail.com>
2024-10-24 12:32:07 +0200

    ej4

--- DEC/Eventos/ej4.js
+++ DEC/Eventos/ej4.js
@@ -1,3 +1,22 @@
-document.write("<table>")
+document.write("<table border=1px>")
 
-for()
+for(i=0; i<=100; i++){
+    document.write("<tr width=5px height = 5px>")
+    for(j=0; j<=100; j++){
+        document.write("<td width=5px height = 5px></td>")
+    }
+    document.write("</tr>")
+}
+
+document.write("</table>")
+
+const tds = document.querySelectorAll("td");
+
+tds.forEach(td => {td.addEventListener("mouseover", function(event) {
+        if (event.altKey) {
+            td.style.backgroundColor = "red";
+        } else if (event.shiftKey) {
+            td.style.backgroundColor = "blue";
+        }
+    });
+});
